4dd2e743d55d6c0a372e3967548feebac45fc1c6
Ray Suyoto <ray.suy@icloud.com>
2018-04-10 09:56:35 -0600

    begin chat window component

---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Segment, Header, Form, TextArea, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setFlash } from '../actions/flash';
+import { addMessage } from '../actions/messages';
+import ChatMessage from './ChatMessage';
+
+class ChatWindow extends React.Component {
+  render() {
+    return null
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    messages: state.message
+  }
+}
+
+export default connect(mapStateToProps)(ChatWindow);
